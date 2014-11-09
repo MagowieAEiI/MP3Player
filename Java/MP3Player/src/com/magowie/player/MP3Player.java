27c08f51594f6094cd395c8cc89111e94920d1c6
kolarz3 <arturmiks@gmail.com>
2014-11-09 17:01:08 +0100

    Projekt dla Eclipse

---
+++ Java/MP3Player/src/com/magowie/player/MP3Player.java
@@ -0,0 +1,16 @@
+package com.magowie.player;
+
+public class MP3Player 
+{
+
+	public MP3Player() 
+	{
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Witaj w odtwarzaczu :DDD!!!!");
+
+	}
+
+}

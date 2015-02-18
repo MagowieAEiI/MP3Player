4312abcb3d3616873318602b450c1ba9d164b423
kolarz3 <arturmiks@gmail.com>
2015-02-18 16:35:39 +0100

    Nowy rozdanie
    Nowy kod

---
+++ C#/MP3PlayerProject/MP3PlayerProject/ComponentControl/Bar.xaml.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Drawing;
+using System.Security.AccessControl;
+using Image = System.Drawing.Image;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+
+namespace MP3PlayerProject.ComponentControl
+{
+    /// <summary>
+    /// Kod jest dość pogmatwany, ważne że, działa
+    /// </summary>
+    public partial class Bar : UserControl
+    {
+        private bool play;
+        private DispatcherTimer timer;
+
+        private int currentTimeMin;
+        private int currentTimeSec;
+        private int allTimeMin;
+        private int allTimeSec;
+        private Image fullBarImage;
+        private Image emptyBarImage;
+
+        public void SetTime(int pCurrentTimeMin,int pCurrentTimeSec,int pAllTimeMin,int pAllTimeSec)
+        {
+            currentTimeMin = pCurrentTimeMin;
+            currentTimeSec = pCurrentTimeSec;
+            allTimeMin = pAllTimeMin;
+            allTimeSec = pAllTimeSec;
+            CurrentTimeLabel.Content = currentTimeMin.ToString()+":"+currentTimeSec.ToString();
+            LongOfSongLabel.Content = allTimeMin.ToString() + ":" + allTimeSec.ToString();
+        }
+
+        public void StartAnimation()
+        {
+            play = true;
+            timer.Start();
+        }
+
+        public void StopAnimation()
+        {
+            timer.Stop();
+        }
+        public Bar()
+        {
+            InitializeComponent();
+            fullBarImage = Image.FromFile(@"../Assets/ControlImages/Bar/Bar_full.png");
+            emptyBarImage = Image.FromFile(@"../Assets/ControlImages/Bar/Bar_empty.png");
+            play = false;
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.05);
+            timer.Tick += playAnimation;
+        }
+
+        /// <summary>
+        /// Skleja odpowiednio pasek postępu
+        /// </summary>
+        private unsafe void SetBarImageCorrect()
+        {
+            Bitmap toRetBitmap = new Bitmap(fullBarImage.Width, fullBarImage.Height, fullBarImage.PixelFormat);
+            Graphics g = Graphics.FromImage(toRetBitmap);
+            float stosunek = (float)((float)(currentTimeMin * 60) + (float)(currentTimeSec) + pom) / (float)(allTimeMin * 60 + allTimeSec);
+            g.DrawImageUnscaledAndClipped(emptyBarImage, new System.Drawing.Rectangle( 0, 0, fullBarImage.Width, fullBarImage.Height));            
+            g.DrawImageUnscaledAndClipped(fullBarImage, new System.Drawing.Rectangle(0, 0, (int)((float)fullBarImage.Width * stosunek), fullBarImage.Height));
+            BitmapImage helpImage = Bitmap2BitmapImage(toRetBitmap); 
+            BarImg.Source = helpImage;
+        }
+
+        private int ile=0;
+        private float pom = 0.0f;
+        private void playAnimation(object obj, EventArgs ea)
+        {
+            //kod jest tak pogmatwany tu ponieważ nie udaje mi się ustawić prawidłowo punktu wobec którego następuje translacja, automatycznie jest to środek grida
+            SetBarImageCorrect();
+            int longOfBar = (int)KnobImg.Width;
+            float x = (float)((float)(currentTimeMin*60) + (float)(currentTimeSec)+pom)/(float)(allTimeMin*60 + allTimeSec);
+            float y;//pomocnicza
+            if (x >= 0.5)
+            {
+                y = x - (float)0.5;
+                y *= 2;
+                KnobImg.RenderTransform = new TranslateTransform((BarImg.ActualWidth/2)*y, BarImg.ActualHeight / 15);
+            }
+            else if (x < 0.5)
+            {
+                y = (float) 0.5 - x;
+                y *= 2;
+                KnobImg.RenderTransform = new TranslateTransform(-(BarImg.ActualWidth / 2) * y, BarImg.ActualHeight / 15);
+            }
+            if (currentTimeSec < 10) CurrentTimeLabel.Content = currentTimeMin.ToString() + ":0" + currentTimeSec.ToString();
+            else CurrentTimeLabel.Content = currentTimeMin.ToString() + ":" + currentTimeSec.ToString();
+            if (currentTimeMin == allTimeMin && currentTimeSec == allTimeSec) {StopAnimation();play=false;}
+            pom += 0.05f;
+            if (pom >= 1.0f)
+            {
+                currentTimeSec++;
+                pom -= 1.0f;
+            }
+            if (currentTimeSec == 60)
+            {
+                currentTimeMin++;
+                currentTimeSec = 0;
+            }
+
+            // int x=
+        }
+
+        private void BarImg_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+           // KnobImg.RenderTransformOrigin=new Point(-e.NewSize.Width/2,e.NewSize.Height/2);
+           // KnobImg.RenderTransform=new TranslateTransform(-e.NewSize.Width/4,e.NewSize.Height/15);
+        }
+
+        private void BarImg_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            double g=e.GetPosition(BarImg).X;
+            double j = (g / BarImg.ActualWidth) * (float)(allTimeMin * 60 + allTimeSec);
+            currentTimeMin = (int)j/60;
+            //j = (int)j/60;
+            currentTimeSec = (int) j%60;
+            if(play==false)StartAnimation();
+        }
+
+        private int pom2 = 0;
+
+        private void KnobImg_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                StopAnimation();
+                double g = e.GetPosition(BarImg).X;
+                if (g < 0||g>BarImg.ActualWidth) return;//zabezpieczenie by nie wyjechać knobem po za skalę
+                double j = (g / BarImg.ActualWidth) * (float)(allTimeMin * 60 + allTimeSec);
+                currentTimeMin = (int)j / 60;
+                //j = (int)j/60;
+                currentTimeSec = (int)j % 60;
+                pom -= 0.05f;
+                playAnimation(null,null);
+                pom2 = 1;
+            }
+            else if (pom2 == 1&&play==true)
+            {
+                pom2 = 0;
+                StartAnimation();
+            }
+        }
+
+        private void KnobImg_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (pom2 == 1 && play == true)
+            {
+                pom2 = 0;
+                StartAnimation();
+            }
+        }
+
+        private void KnobImg_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (pom2 == 1 && play == true)
+            {
+                pom2 = 0;
+                StartAnimation();
+            }
+        }
+
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern bool DeleteObject(IntPtr hObject);
+
+        private BitmapImage Bitmap2BitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bitmap.Save(stream, ImageFormat.Png); // Was .Bmp, but this did not show a transparent background.
+
+                stream.Position = 0;
+                BitmapImage result = new BitmapImage();
+                result.BeginInit();
+                // According to MSDN, "The default OnDemand cache option retains access to the stream until the image is needed."
+                // Force the bitmap to load right now so we can dispose the stream.
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.StreamSource = stream;
+                result.EndInit();
+                result.Freeze();
+                return result;
+            }
+        }
+
+    }
+}
---
+++ C#/MP3PlayerProject/MP3PlayerProject/ComponentControl/StarPanel.xaml.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MP3PlayerProject.ComponentControl
+{
+    /// <summary>
+    /// Interaction logic for StarPanel.xaml
+    /// </summary>
+    public partial class StarPanel : UserControl
+    {
+        public int starNumber;//jest to liczba gwiazdek jaką ma dana piosenka
+        public StarPanel()
+        {
+            InitializeComponent();
+            starNumber = 0;
+            Grid_MouseLeave(null,null);
+        }
+
+        private void starMouseEnter(object sender, MouseEventArgs e)
+        {
+            //CzyscGwiazdki();
+            if (sender.Equals(star0))
+            {
+                star0.ImageSourceCurrent = star0.ImageSourceMouseIn;
+                star0.Img1.Source = star0.ImageSourceCurrent;
+
+                star1.ImageSourceCurrent = star1.ImageSourceMouseOut;
+                star1.Img1.Source = star1.ImageSourceCurrent;
+
+                star2.ImageSourceCurrent = star2.ImageSourceMouseOut;
+                star2.Img1.Source = star2.ImageSourceCurrent;
+
+                star3.ImageSourceCurrent = star3.ImageSourceMouseOut;
+                star3.Img1.Source = star3.ImageSourceCurrent;
+
+                star4.ImageSourceCurrent = star4.ImageSourceMouseOut;
+                star4.Img1.Source = star4.ImageSourceCurrent;
+            }
+            else if (sender.Equals(star1))
+            {
+                star0.ImageSourceCurrent = star0.ImageSourceMouseIn;
+                star0.Img1.Source = star0.ImageSourceCurrent;
+
+                star1.ImageSourceCurrent = star1.ImageSourceMouseIn;
+                star1.Img1.Source = star1.ImageSourceCurrent;
+
+                star2.ImageSourceCurrent = star2.ImageSourceMouseOut;
+                star2.Img1.Source = star2.ImageSourceCurrent;
+
+                star3.ImageSourceCurrent = star3.ImageSourceMouseOut;
+                star3.Img1.Source = star3.ImageSourceCurrent;
+
+                star4.ImageSourceCurrent = star4.ImageSourceMouseOut;
+                star4.Img1.Source = star4.ImageSourceCurrent;
+            }
+            else if (sender.Equals(star2))
+            {
+                star0.ImageSourceCurrent = star0.ImageSourceMouseIn;
+                star0.Img1.Source = star0.ImageSourceCurrent;
+
+                star1.ImageSourceCurrent = star1.ImageSourceMouseIn;
+                star1.Img1.Source = star1.ImageSourceCurrent;
+
+                star2.ImageSourceCurrent = star2.ImageSourceMouseIn;
+                star2.Img1.Source = star2.ImageSourceCurrent;
+
+                star3.ImageSourceCurrent = star3.ImageSourceMouseOut;
+                star3.Img1.Source = star3.ImageSourceCurrent;
+
+                star4.ImageSourceCurrent = star4.ImageSourceMouseOut;
+                star4.Img1.Source = star4.ImageSourceCurrent;
+            }
+            else if (sender.Equals(star3))
+            {
+                star0.ImageSourceCurrent = star0.ImageSourceMouseIn;
+                star0.Img1.Source = star0.ImageSourceCurrent;
+
+                star1.ImageSourceCurrent = star1.ImageSourceMouseIn;
+                star1.Img1.Source = star1.ImageSourceCurrent;
+
+                star2.ImageSourceCurrent = star2.ImageSourceMouseIn;
+                star2.Img1.Source = star2.ImageSourceCurrent;
+
+                star3.ImageSourceCurrent = star3.ImageSourceMouseIn;
+                star3.Img1.Source = star3.ImageSourceCurrent;
+
+                star4.ImageSourceCurrent = star4.ImageSourceMouseOut;
+                star4.Img1.Source = star4.ImageSourceCurrent;
+            }
+            else if (sender.Equals(star4))
+            {
+                star0.ImageSourceCurrent = star0.ImageSourceMouseIn;
+                star0.Img1.Source = star0.ImageSourceCurrent;
+
+                star1.ImageSourceCurrent = star1.ImageSourceMouseIn;
+                star1.Img1.Source = star1.ImageSourceCurrent;
+
+                star2.ImageSourceCurrent = star2.ImageSourceMouseIn;
+                star2.Img1.Source = star2.ImageSourceCurrent;
+
+                star3.ImageSourceCurrent = star3.ImageSourceMouseIn;
+                star3.Img1.Source = star3.ImageSourceCurrent;
+
+                star4.ImageSourceCurrent = star4.ImageSourceMouseIn;
+                star4.Img1.Source = star4.ImageSourceCurrent;
+            }
+        }
+
+        private void starMouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+
+        private void CzyscGwiazdki()
+        {
+            star0.ImageSourceCurrent = star0.ImageSourceMouseOut;
+            star0.Img1.Source = star0.ImageSourceCurrent;
+            star1.ImageSourceCurrent = star1.ImageSourceMouseOut;
+            star1.Img1.Source = star1.ImageSourceCurrent;
+            star2.ImageSourceCurrent = star2.ImageSourceMouseOut;
+            star2.Img1.Source = star2.ImageSourceCurrent;
+            star3.ImageSourceCurrent = star3.ImageSourceMouseOut;
+            star3.Img1.Source = star3.ImageSourceCurrent;
+            star4.ImageSourceCurrent = star4.ImageSourceMouseOut;
+            star4.Img1.Source = star4.ImageSourceCurrent;
+        }
+        private void Grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            CzyscGwiazdki();
+            if (starNumber >= 1)
+            {
+                star0.ImageSourceCurrent = star0.ImageSourceMouseIn;
+                star0.Img1.Source = star0.ImageSourceCurrent;
+            }
+            if (starNumber >= 2)
+            {
+                star1.ImageSourceCurrent = star1.ImageSourceMouseIn;
+                star1.Img1.Source = star1.ImageSourceCurrent;
+            }
+            if (starNumber >= 3)
+            {
+                star2.ImageSourceCurrent = star2.ImageSourceMouseIn;
+                star2.Img1.Source = star2.ImageSourceCurrent;
+            }
+            if (starNumber >= 4)
+            {
+                star3.ImageSourceCurrent = star3.ImageSourceMouseIn;
+                star3.Img1.Source = star3.ImageSourceCurrent;
+            }
+            if (starNumber >= 5)
+            {
+                star4.ImageSourceCurrent = star4.ImageSourceMouseIn;
+                star4.Img1.Source = star4.ImageSourceCurrent;
+            }
+        }
+
+        private void starMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender.Equals(star0))
+            {
+                starNumber = 1;
+            }
+            else if (sender.Equals(star1))
+            {
+                starNumber = 2;
+            }
+            else if (sender.Equals(star2))
+            {
+                starNumber = 3;  
+            }
+            else if (sender.Equals(star3))
+            {
+                starNumber = 4;
+            }
+            else if (sender.Equals(star4))
+            {
+                starNumber = 5;
+            }
+        }
+
+        private void star1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void star0_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+    }
+}
